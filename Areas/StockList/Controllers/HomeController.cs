e48d2d750c2e64fbbe9101a39ab10663933fa0c3
Savjjz <60676237+Savjjz@users.noreply.github.com>
2020-12-14 18:06:24 +0300

    Initial commit

---
+++ Areas/StockList/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockExchangeSimulator.Areas.StockList.Controllers
+{
+    [Area("StockList")]
+    public class HomeController : Controller
+    {
+        [Route("[area]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/UserAccount/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockExchangeSimulator.Areas.UserAccount.Controllers
+{
+    [Area("UserAccount")]
+    public class AccountController : Controller
+    {
+        [Route("[area]/[controller]/[action]")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [Route("[area]/[controller]/[action]")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
